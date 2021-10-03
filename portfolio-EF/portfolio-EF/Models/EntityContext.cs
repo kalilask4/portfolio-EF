2ac4f3c9e6319a8730edc639abcc5f0b5f7a5716
kalilask4 <kalilask4@gmail.com>
2021-10-04 01:04:56 +0300

    add EntityContext

---
+++ portfolio-EF/portfolio-EF/Models/EntityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio_EF.Models
+{
+    class EntityContext : DbContext
+    {
+        public EntityContext() : base("DefaultConnection")
+        {
+          
+        }
+        public DbSet<Coin> Coins { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
